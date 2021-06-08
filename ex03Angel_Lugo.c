b8959f456d3fb9c73e104d9e92ee68bdf0951885
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-07 19:19:51 -0500

    Create ex03Angel_Lugo.c

---
+++ ex03Angel_Lugo.c
@@ -0,0 +1,13 @@
+  #include<stdio.h>
+
+int main(void){
+  float A,   B;
+  printf("Insertar cualquier valor para A\n");
+  scanf("%f", &A);
+
+  B= (A*4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19);
+
+printf("El resultado es %.3f", B);
+
+  return 0;
+}
